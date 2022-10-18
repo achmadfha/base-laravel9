6fedb8e362d2c67cf280c2f129578c200a7470e4
Authinfo <sultanfiki403@gmail.com>
2022-10-18 20:34:51 +0700

    mains
    mains

--- config/fortify.php
+++ config/fortify.php
@@ -135,13 +135,13 @@ return [
         Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirm' => true,
-            'confirmPassword' => true,
-            // 'window' => 0,
-        ]),
+//        Features::updateProfileInformation(),
+//        Features::updatePasswords(),
+//        Features::twoFactorAuthentication([
+//            'confirm' => true,
+//            'confirmPassword' => true,
+//            // 'window' => 0,
+//        ]),
     ],
 
 ];
